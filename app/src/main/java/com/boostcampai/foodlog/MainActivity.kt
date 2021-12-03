9a9f4a09b6be00072d511cd4df55f1dec4461d16
ChoiJaeYeong <chlwodud2@naver.com>
2021-12-04 00:54:50 +0900

    [Feature] 네비게이션 연결 및 Bottom Navigation 설정

--- app/src/main/java/com/boostcampai/foodlog/MainActivity.kt
+++ app/src/main/java/com/boostcampai/foodlog/MainActivity.kt
@@ -2,10 +2,22 @@ package com.boostcampai.foodlog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.boostcampai.foodlog.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+        navController = navHostFragment!!.findNavController()
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/boostcampai/foodlog/adapter/HomeDietRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.boostcampai.foodlog.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.boostcampai.foodlog.databinding.ItemRecyclerHomeDietBinding
+import com.boostcampai.foodlog.model.TodayDietModel
+
+class HomeDietRecyclerAdapter :
+    ListAdapter<TodayDietModel, RecyclerView.ViewHolder>(HomeDietDiffUtil()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return HomeDietViewHolder(
+            ItemRecyclerHomeDietBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = getItem(position)
+        (holder as HomeDietViewHolder).bind(item)
+    }
+
+    inner class HomeDietViewHolder(private val binding: ItemRecyclerHomeDietBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: TodayDietModel) {
+            binding.item = item
+        }
+    }
+}
+
+private class HomeDietDiffUtil : DiffUtil.ItemCallback<TodayDietModel>() {
+    override fun areItemsTheSame(oldItem: TodayDietModel, newItem: TodayDietModel): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: TodayDietModel, newItem: TodayDietModel): Boolean {
+        return oldItem == newItem
+    }
+}
