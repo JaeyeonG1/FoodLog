f632efb96006423cae5f973b8f1a622193224602
hyeonjin <choihj94@gmail.com>
2021-12-14 23:02:59 +0900

    [Feature] Detail Fragment 리사이클러뷰 추가

--- app/src/main/java/com/boostcampai/foodlog/model/DailyDietModel.kt
+++ app/src/main/java/com/boostcampai/foodlog/model/DailyDietModel.kt
@@ -2,7 +2,19 @@ package com.boostcampai.foodlog.model
 
 data class DailyDietModel(
     var date:String,
-    var total:Int,
     var goal:Int,
     var foods:List<Food>,
-)
+){
+    fun foodsToString():String{
+        val str = foods.fold("") { sum, now -> "$sum${now.name}, " }
+        return str.substring(0, str.length - 2)
+    }
+
+    fun getNutritionSumString():String{
+        if (foods.isEmpty())
+            return "0"
+
+        val sum = foods.map { it.nutrition.value }.fold(0f) { sum, now -> sum + now }
+        return sum.toInt().toString() + " " + foods[0].nutrition.unit + " " + "/" + goal.toString() + foods[0].nutrition.unit
+    }
+}
