bc068c33c197d921d6421fd5e9e1dfa6cad3952e
ChoiJaeYeong <chlwodud2@naver.com>
2021-12-19 07:42:57 +0900

    [Refactor] DailyFragment 이미지 로드 Glide로 개선

--- app/src/main/java/com/boostcampai/foodlog/adapter/DailyFoodRecyclerAdapter.kt
+++ app/src/main/java/com/boostcampai/foodlog/adapter/DailyFoodRecyclerAdapter.kt
@@ -1,15 +1,25 @@
 package com.boostcampai.foodlog.adapter
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.boostcampai.foodlog.cropBitmap
 import com.boostcampai.foodlog.databinding.ItemRecyclerDailyFoodBinding
 import com.boostcampai.foodlog.model.DailyFoodModel
 import com.boostcampai.foodlog.model.Food
+import com.boostcampai.foodlog.model.Position
+import com.bumptech.glide.Glide
 
-class DailyFoodRecyclerAdapter(val calcNutrition: (Food) -> String) :
+class DailyFoodRecyclerAdapter(
+    val calcNutrition: (Food) -> String,
+    val uriToBitmap: (String, Position, ImageView) -> (Unit),
+    val context: Context
+) :
     ListAdapter<DailyFoodModel, RecyclerView.ViewHolder>(DailyFoodDiffUtil()) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return DailyFoodViewHolder(
@@ -31,6 +41,7 @@ class DailyFoodRecyclerAdapter(val calcNutrition: (Food) -> String) :
         fun bind(item: DailyFoodModel) {
             binding.item = item
             binding.tvValue.text = calcNutrition(item.food)
+            uriToBitmap(item.uri, item.food.pos, binding.ivFood)
         }
     }
 }
